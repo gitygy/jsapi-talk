a31dc616d8a917a9aaf027312c3516df72ae31a8
Nick Senger <dev@nsenger.com>
2017-06-07 15:55:53 -0700

    feat: Lints the TypeScript files

--- demos/Step5_LayerFilter/ts/main.ts
+++ demos/Step5_LayerFilter/ts/main.ts
@@ -1,31 +1,31 @@
-import * as watchUtils from 'esri/core/watchUtils';
-import * as Map from 'esri/Map';
-import * as FeatureLayer from 'esri/layers/FeatureLayer';
-import * as MapView from 'esri/views/MapView';
-import * as PopupTemplate from 'esri/PopupTemplate';
-import * as UniqueValueRenderer from 'esri/renderers/UniqueValueRenderer';
-import * as SimpleMarkerSymbol from 'esri/symbols/SimpleMarkerSymbol';
-import * as Color from 'esri/Color';
+import * as Color from "esri/Color";
+import * as watchUtils from "esri/core/watchUtils";
+import * as FeatureLayer from "esri/layers/FeatureLayer";
+import * as Map from "esri/Map";
+import * as PopupTemplate from "esri/PopupTemplate";
+import * as UniqueValueRenderer from "esri/renderers/UniqueValueRenderer";
+import * as SimpleMarkerSymbol from "esri/symbols/SimpleMarkerSymbol";
+import * as MapView from "esri/views/MapView";
 import * as Popup from "esri/widgets/Popup";
 
 // Symbol for beaches with Lifeguards
-var lifeSym = new SimpleMarkerSymbol({
-  size: 14,
+const lifeSym = new SimpleMarkerSymbol({
   color: new Color("#4AB541"),
   outline: { // Autocasts as new SimpleLineSymbol()
     color: new Color([255, 255, 255, 0.50]), // Autocasts as new Color()
     width: 2
-  }
+  },
+  size: 14
 });
 
 // Symbol for beaches without Lifeguards
-var nolifeSym = new SimpleMarkerSymbol({
-  size: 14,
+const nolifeSym = new SimpleMarkerSymbol({
   color: new Color("#E17D1E"),
   outline: { // Autocasts as new SimpleLineSymbol()
     color: new Color([255, 255, 255, 0.50]), // Autocasts as new Color()
     width: 2
-  }
+  },
+  size: 14
 });
 
 /******************************************************************
@@ -38,92 +38,83 @@ var nolifeSym = new SimpleMarkerSymbol({
   *
   ******************************************************************/
 
-var beachRenderer = new UniqueValueRenderer({
-  defaultSymbol: lifeSym,
+const beachRenderer = new UniqueValueRenderer({
   defaultLabel: "Beaches with lifeguards",
+  defaultSymbol: lifeSym,
   field: "Lifeguards",
   uniqueValueInfos: [{
-    value: "Y",
+    label: "Beaches with lifeguards",
     symbol: lifeSym,
-    label: "Beaches with lifeguards"
+    value: "Y"
   }, {
-    value: "N",
+    label: "Beaches without lifeguards on duty",
     symbol: nolifeSym,
-    label: "Beaches without lifeguards on duty"
+    value: "N"
   }]
 });
 
-var popupTemplate = new PopupTemplate({
-  title: "<b>Beach: {NAME}</b>",
+const popupTemplate = new PopupTemplate({
   // Step 2: Specify the content, first set the display fields
   content: [{
-    type: "fields",
     fieldInfos: [{
       fieldName: "ADDRESS",
-      visible: true,
-      label: "Address: "
+      label: "Address: ",
+      visible: true
     }, {
       fieldName: "Lifeguards",
-      visible: true,
-      label: "Lifeguards on duty: "
+      label: "Lifeguards on duty: ",
+      visible: true
     }, {
       fieldName: "Swimming",
-      visible: true,
-      label: "Swimming allowed: "
+      label: "Swimming allowed: ",
+      visible: true
     }, {
       fieldName: "Surfing",
-      visible: true,
-      label: "Surfing allowed: "
+      label: "Surfing allowed: ",
+      visible: true
     }, {
       fieldName: "URL",
-      visible: true,
-      label: "Beach website with additional info: "
-    }]
+      label: "Beach website with additional info: ",
+      visible: true
+    }],
+    title: "<b>Beach: {NAME}</b>",
+    type: "fields"
   }, {
     // Step 3: Specify the second type of popup element to display, in this case, images
     // there is an "image" field in the feature service that points to an online image
-    type: "media",
     mediaInfos: [{
       type: "image",
       value: {
         sourceURL: "{Image}"
       }
-    }]
+    }],
+    type: "media",
   }]
 });
 
 // Create beaches featurelayer and set the renderer on the layer
-var beaches = new FeatureLayer({
-  url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/San_Diego_Beaches/FeatureServer/0",
-
-  // Specify the outfields for the featurelayer in addition to passing in the template created above
+const beaches = new FeatureLayer({
   outFields: ["*"],
-  popupTemplate: popupTemplate,
-
-  // set renderer
-  renderer: beachRenderer
+  popupTemplate,
+  renderer: beachRenderer,
+  url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/San_Diego_Beaches/FeatureServer/0",
 });
 
 // Create Neighborhoods featurelayer and set opacity on layer
-var hoods = new FeatureLayer({
-  url: "http://services.arcgis.com/OUDgwkiMsqiL8Tvp/arcgis/rest/services/NewSDNeighborhoods/FeatureServer/0",
-  // set opacity
-  opacity: 0.50
+const hoods = new FeatureLayer({
+  opacity: 0.50,
+  url: "http://services.arcgis.com/OUDgwkiMsqiL8Tvp/arcgis/rest/services/NewSDNeighborhoods/FeatureServer/0"
 });
 
-var map = new Map({
+const map = new Map({
   basemap: "topo",
   layers: [beaches, hoods]
 });
 
-var view = new MapView({
-  container: "viewDiv",
-  map: map,
+const view = new MapView({
   center: [-117.16866016384272, 32.776725339767964],
-
-  //Set the popup property in the MapView so that docking is enabled
-  // and dockoptions are set. In this case, the popup can be docked to the
-  //bottom right of the application.
+  container: "viewDiv",
+  map,
   popup: new Popup({
     dockEnabled: true,
     dockOptions: {
@@ -140,22 +131,22 @@ view.ui.add(document.getElementById("container"), "top-right");
  ******************************************************************/
 
 let hoodsLayerView: __esri.FeatureLayerView;
-var featuresMap = {};
+const featuresMap = {};
 
-view.whenLayerView(hoods).then(function(lyrView: __esri.FeatureLayerView) {
+view.whenLayerView(hoods).then((lyrView: __esri.FeatureLayerView) => {
   hoodsLayerView = lyrView;
   // Step 2: Make sure that the layer is not updating and currently fetching data
   return watchUtils.whenFalseOnce(hoodsLayerView, "updating");
 })
-  .then(function() {
+  .then(() => {
     // Step 3: Query all features in the layerview and return the results
     return hoodsLayerView.queryFeatures();
   })
-  .then(function(features) {
+  .then((features) => {
     // Step 4: Build a dropdown for each unique value in Neighborhood field
-    features.forEach(function(feature: __esri.Graphic) {
-      var featureId = feature.attributes.OBJECTID_1;
-      var uniqueVal = feature.attributes.NAME;
+    features.forEach((feature: __esri.Graphic) => {
+      const featureId = feature.attributes.OBJECTID_1;
+      const uniqueVal = feature.attributes.NAME;
       const option = document.createElement("option");
       option.value = featureId;
       option.innerHTML = uniqueVal;
@@ -167,10 +158,10 @@ view.whenLayerView(hoods).then(function(lyrView: __esri.FeatureLayerView) {
 
 // Step 4: Listen for the change event on the dropdown
 // and set the layer's definition expression to the chosen value
-var select = document.getElementById("selectNeighborhood") as HTMLInputElement;
+const select = document.getElementById("selectNeighborhood") as HTMLInputElement;
 select.onchange = (e) => {
-  var featureId = select.value;
-  var expr = select.value === "" ? "" : "OBJECTID_1 = '" + featureId + "'";
+  const featureId = select.value;
+  const expr = select.value === "" ? "" : "OBJECTID_1 = '" + featureId + "'";
   hoods.definitionExpression = expr;
 
   // Navigate to the selected feature;
--- demos/Step7_Widgets/ts/main.ts
+++ demos/Step7_Widgets/ts/main.ts
@@ -1,6 +1,6 @@
-import * as WebMap from "esri/WebMap";
-import * as MapView from "esri/views/MapView";
 import * as watchUtils from "esri/core/watchUtils";
+import * as MapView from "esri/views/MapView";
+import * as WebMap from "esri/WebMap";
 import * as Legend from "esri/widgets/Legend";
 
 const map = new WebMap({
@@ -23,40 +23,38 @@ view.ui.add(document.getElementById("container"), "top-right");
  *
  ******************************************************************/
 
-view.then(function() {
-var beaches = map.layers.getItemAt(1);
-// Step 1: Create the widget
-var legend = new Legend({
-    // Step 2: Specify any additional properties for the legend. In this case,
-    // we are just setting the view to where the legend should apply
-    view: view,
-    layerInfos: [{
-    layer: beaches,
-    title: "San Diego beaches"
-    }]
+view.then(() => {
+    const beaches = map.layers.getItemAt(1);
+    // Step 1: Create the widget
+    const legend = new Legend({
+        // Step 2: Specify any additional properties for the legend. In this case,
+        // we are just setting the view to where the legend should apply
+        view,
+        layerInfos: [{
+            layer: beaches,
+            title: "San Diego beaches"
+        }]
+    });
+    // Step 3: Add the widget to the view's UI, specify the docking position as well
+    view.ui.add(legend, "bottom-right");
 });
-// Step 3: Add the widget to the view's UI, specify the docking position as well
-view.ui.add(legend, "bottom-right");
-});
-var hoodsLayerView: __esri.FeatureLayerView;
-var featuresMap = {};
+let hoodsLayerView: __esri.FeatureLayerView;
+const featuresMap = {};
 
-view.then(function() {
-    var hoods = map.layers.getItemAt(0);
+view.then(() => {
+    const hoods = map.layers.getItemAt(0);
 
     view.whenLayerView(hoods)
-        .then(function(lyrView) {
+        .then((lyrView) => {
             hoodsLayerView = lyrView;
             // Make sure that the layer is not updating and currently fetching data
             return watchUtils.whenFalseOnce(hoodsLayerView, "updating");
-        }).then(function() {
-        // Step 3: Query all features in the layerview and return the results
-            return hoodsLayerView.queryFeatures();
-        }).then(function(features) {
+        }).then(() => hoodsLayerView.queryFeatures())
+        .then((features) => {
             //  Build a dropdown for each unique value in Neighborhood field
-            features.forEach(function(feature: __esri.Graphic) {
-                var featureId = feature.attributes.OBJECTID_1;
-                var uniqueVal = feature.attributes.NAME;
+            features.forEach((feature: __esri.Graphic) => {
+                const featureId = feature.attributes.OBJECTID_1;
+                const uniqueVal = feature.attributes.NAME;
                 const option = document.createElement("option");
                 option.value = featureId;
                 option.innerHTML = uniqueVal;
@@ -68,12 +66,11 @@ view.then(function() {
 
     // Listen for the change event on the dropdown
     // and set the layer's definition expression to the chosen value
-    var select = document.getElementById("selectNeighborhood") as HTMLInputElement;
+    const select = document.getElementById("selectNeighborhood") as HTMLInputElement;
     select.onchange = (e) => {
-        var featureId = select.value;
-        var expr = select.value === "" ? "" : "OBJECTID_1 = '" + featureId + "'";
+        const featureId = select.value;
+        const expr = select.value === "" ? "" : "OBJECTID_1 = '" + featureId + "'";
         hoods.definitionExpression = expr;
-
         // Navigate to the selected feature;
         view.goTo(featuresMap[featureId]);
     };
